55782a4d49b31bc51b9a797ff107d487f6ca0c0c
hyunsss <mh81hj1@naver.com>
2023-11-29 15:02:57 +0900

    Dice 확률 계산

--- Dice.cs
+++ Dice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,23 +10,59 @@ namespace DiceRPG
     public class Dice
     {
         List<Skill> DiceSkills = new List<Skill>();
+        int[] DiceNumberPer = new int[1200]; // 다이스 확률 분포
+        float[] DicePer = new float[6];   //다이스 확률 계산
 
 
-        public void DicePercent()
+
+        private void DicePercent()
         {
-            int[] DicePer = new int[1000];
-
             int num = 1;
-            for(int i = 1; i <= DicePer.Length; i++)
+            for(int i = 1; i <= DiceNumberPer.Length; i++)
             {
-                i = num;
-                if(i - 1 != 0 && i % 200 == 0)
+                DicePer[i] = num;
+                if(i - 1 != 0 && i % 200 == 0 && num <= 7)
                 {
                     num++;
                 }
             }
-
         }
+
+        public void NewDicePer(int DiceNum, int Percent) {
+            
+            for(int i = 0; i < 5; i++) {
+                int temp = Percent / 5;
+                int num = 1;
+                for(int DiceIndex = 0; DiceIndex < DiceNumberPer.Length; DiceIndex++ ) {
+                    if(num == DiceNum) {
+                        break;
+                    }
+                    if(temp != 0 && DiceNumberPer[DiceIndex] == num) {
+                        temp--;
+                        DiceNumberPer[DiceIndex] = DiceNum;
+                    }
+                }
+                num++;
+            }
+        }
+        
+        private void DiceCurculate() {
+            int temp = 0;
+            for(int i = 0; i < 6; i++) {
+                for(int DiceIndex = 0; DiceIndex < DiceNumberPer.Length; DiceIndex++) {
+                    if(DiceNumberPer[DiceIndex] == i) {
+                        temp++;
+                    }
+                }
+
+                DicePer[i] = (temp / DiceNumberPer.Length) * 100;
+            }
+        }
+
+        public float DiceRoad(int DiceIndex) {
+            return DicePer[DiceIndex - 1];
+        }
+        
     }
 
     public class Skill
